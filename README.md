09685eb4d5873f2e8821c9138b5e315a6ffb6f8a
Daniel <danielt.greiner@gmail.com>
2021-09-11 16:07:57 -0700

    Created html, css and js files. Began creating grid for main part of etch-a-sketch. Need to debug and understand why grid elements are visible in Inspector, but not in browser window

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Etch-a-Sketch
+
+The goal of this project is to make an 
+etch-a-sketch like interface. I hope to
+use this project to improve my CSS skills
+as well as learn more about DOM manipulation.
---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container");
+
+
+for (i = 0; i < 16; i++) {
+    for (j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.classList.add('.gridBox');
+        container.appendChild(div);
+    }
+}
