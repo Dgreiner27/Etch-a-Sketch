ea153420ace91eae003d4deeb2642ac9408845da
Daniel <danielt.greiner@gmail.com>
2021-09-13 10:29:48 -0700

    fixed the grid so there are now a6 rows and columns being generated by JS. Set up a hover effect to change backgroud  color of gridBox using CSS. May need to change to be a JS effect

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const container = document.querySelector("#container");
 
 
+
+
+
+
+
+
+
+const container = document.querySelector('#container');
 for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
         const div = document.createElement('div');
-        div.classList.add('.gridBox');
+        div.classList.add('gridBox');
+        
         container.appendChild(div);
     }
 }
+
