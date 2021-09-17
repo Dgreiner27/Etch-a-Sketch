016b5bfea81f94770400cc76e7e899904619a912
Daniel <danielt.greiner@gmail.com>
2021-09-16 19:56:25 -0700

    Solved the issue ofAll event handlers being within a for loop.  Still running into error while reloading with new grid size. Generates correct size and spacing, but number of grid elements is off

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
+const defaultSize = 16;
+let currentSize = defaultSize;
 
 
 
+function gridResizer(squares) {
+    let containerHeight = window.getComputedStyle(document.documentElement).getPropertyValue('--containerHeight');
+    let newSquareSize = (parseInt(containerHeight)-2*squares)/squares;
+    document.documentElement.style.setProperty('--gridBoxHeight', `${newSquareSize}px`);
+    return newSquareSize;
+}
 
 
 
-
+function clearGrid() {
+        const gridItems = document.querySelectorAll('#container > div');
+        gridItems.forEach((item) => {
+            item.classList.remove('gridBoxMousedOver');
+        });
+    }
 
 
 const container = document.querySelector('#container');
 const clearButton = document.querySelector('#clear');
 
-for (i = 0; i < 256; i++) {
-    
+
+function createGrid(size) {
+    for (let i = 0; i < size*size; i++) {
         const div = document.createElement('div');
         div.classList.add('gridBox');
         container.appendChild(div);
-        console.log(div);
         div.addEventListener("mouseover", () => {
             div.classList.add('gridBoxMousedOver');    
         });
-
         
-        clearButton.addEventListener("click", () => {
-            div.classList.remove('gridBoxMousedOver');
-
-        });
-    
+    }
 }
 
+clearButton.addEventListener("click", () => {
+    clearGrid();
+    currentSize = prompt('Size?');
+    gridResizer(currentSize);
+    createGrid(currentSize);
+});   
+
+createGrid(currentSize);
+
+/* Can I just change const div to var?
+Will that push its scope outsode of the function? */
